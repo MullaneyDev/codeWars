e297e9a16eb693194b17e4dd38e5ead483d171c5
MullaneyDev <matt.mullaney@live.co.uk>
2023-09-07 08:36:58 +0100

    Addition of two more solutions from Codewars tasks

---
+++ 8kyuSolutions/multiplication.js
@@ -0,0 +1,15 @@
+// Problem
+function simpleMultiplication(number) {
+  // your code........
+}
+
+
+//Solution
+function simpleMultiplication(number) {
+  if (number % 2 == 0){
+    answer = number * 8;
+  } else {
+    answer = number * 9;
+  } return answer;
+};
+
---
+++ 8kyuSolutions/reverseString.js
@@ -0,0 +1,12 @@
+// Problem
+
+function solution(str) {
+
+}
+
+
+
+//Solution
+function solution(str) {
+  return str.split("").reverse().join("");
+}
